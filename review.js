b652fc15f2ba35a31ee9380f5ba50578e1f82e32
Jordan Smith <Jordansmithformally@gmail.com>
2024-10-30 14:09:43 -0400

    basics

---
+++ review.js
@@ -0,0 +1,32 @@
+x = 123 // assigning a value(123) to the varible(x)
+let y = 456 // set initial value
+y = 789 // changed the value
+
+const TAX_RATE = 0.88 // capitals are used to indicate its a constant(unchanging)
+TAX_RATE = 0.12 // error because const means it wont change 
+
+const list = [1,2];
+list.push(3); //legal because the .push allows me to add to it
+
+var foo = "bar" // old school - do not use - stick to let and const
+
+
+// let is used when we want to assign a value that we can change, const is for unchangeable values
+
+const person = {first: "Jordan", last: "smith"};
+person.age = 57; // legal because of the .age
+person = {} // illegal becuase its already declared on line 16
+
+
+// DATA TYPES
+first = "Jordan" // String 
+age = 57 // Number
+DOB = new Date("9/13/1967"); // Date
+isCool = true // Boolean - true/false
+let u; // undefined because it hasnt been assigned a value let u = 55 would
+
+
+// COMPLEX DATA TYPES
+list = [111,222,333] // Array is an ordered list 
+dict = {color:"red", width:"100px"} // Object (Associative Array)
+
